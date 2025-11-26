a30bc03abe0cf7cffab52055628dd24101b1c3d5
Rina-ui <marinagracia895@gmail.com>
2025-11-26 09:48:51 +0100

    initialisation du fichier control

--- control.py
+++ control.py
@@ -1,1 +1,18 @@
+import json
+from manager import frequency, threshold, running
 
+async def handle_message(data, ws_manager):
+    global frequency, threshold, running
+    message = json.loads(data)
+
+    if message["action"] == "SET_FREQUENCY":
+        frequency = float(message["value"])
+    elif message["action"] == "SET_THRESHOLD":
+        threshold = int(message["value"])
+    elif message["action"] == "STOP":
+        running = False
+    elif message["action"] == "START":
+        running = True
+
+    # Confirmation send au client
+    await ws_manager.broadcast({"type": "control", "status": message})
